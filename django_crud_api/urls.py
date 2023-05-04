7182a0be6b2132699baec4bf00d2c1c7b6707127
Juan Valencia <juanfvasa@gmail.com>
2023-05-04 01:45:12 -0500

    Explaining the tasks path

--- django_crud_api/urls.py
+++ django_crud_api/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls')),
+    path('tasks/', include('tasks.urls')), # Para incluir al path task las rutas del archivo urls de la app tasks
 ]
